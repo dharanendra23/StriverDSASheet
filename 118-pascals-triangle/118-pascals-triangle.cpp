1059ecc756b2e11eafa4ed4282e3a7ec508edbe5
Dharanendra L V <dharanendra.lv100@gmail.com>
2022-05-09 10:42:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.59%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,22 +5,14 @@ public:
         vector<vector<int> > tri(numRows);
         for(int i = 0; i < numRows; i++) {
             
-            if(i == 0) {
-                
-                tri[i].push_back(1);
-            } else if(i == 1) {
-                
-                tri[i].push_back(1);
-                tri[i].push_back(1);
-            } else {
-               
-                tri[i].push_back(1);
-                for(int j = 0; j < i-1; j++) {
+            tri[i].push_back(1);
+           
+            for(int j = 0; j < i-1; j++) {
                     
-                    tri[i].push_back(tri[i-1][j]+tri[i-1][j+1]);
-                }
-                tri[i].push_back(1);
+                tri[i].push_back(tri[i-1][j]+tri[i-1][j+1]);
             }
+            
+            if(i) tri[i].push_back(1);
         }
         
         return tri;
